559f8da8fc1b789428eeca594d5c95b27dc3df1e
theeasywayup <canadahamit24@gmail.com>
2026-01-08 22:36:21 -0500

    Run 5 times now

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Hello World");
             Thread.sleep(1000);
         }
